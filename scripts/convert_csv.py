ddfcbd512fca3c10d3a0b38498be33f1f0de565d
andrelamor <amorim.andrel@gmail.com>
2024-07-08 10:41:56 -0300

    modifica script para excluir cabeçalho e linhas duplicatas

--- scripts/convert_csv.py
+++ scripts/convert_csv.py
@@ -4,6 +4,7 @@ import re
 from unidecode import unidecode
 import config
 
+
 def find_all_files():
     upload_files = os.listdir('upload')
     for file in upload_files:
@@ -15,45 +16,40 @@ def find_all_files():
             else:
                 convert_csv(file)
 
+
 def convert_csv(file, aba=None):
-    read_file = pd.read_excel (f'upload/{file}', aba)
-    read_file = list(read_file.items())[0][1] if aba == None else read_file
-    read_file = read_file \
-                .replace('\n', ' ', regex=True) \
-                .replace('\r', '', regex=True)
-    columns = read_file.columns
-    new_columns = []
-    for column in columns:
-        new_columns.append(snake_small_case(column))
-    # import ipdb; ipdb.set_trace(context=10)
-    csv_name = file.split('.xls')[0]
-    csv_name = csv_name if aba == None else f'{aba}' # to xlsx and xls
-    read_file.columns=new_columns
+    read_file = pd.read_excel(f'upload/{file}', aba)
+    read_file = list(read_file.items())[0][1] if aba is None else read_file
 
-    # Remove white spaces from the beginning and the end of all columns
+    # Remove duplicate rows (keep the last occurrence)
+    read_file = read_file.loc[~read_file.duplicated(keep='last')]
+
+    # Replace newlines and carriage returns with spaces
+    read_file = read_file.replace('\n', ' ', regex=True).replace('\r', '', regex=True)
+
+    # Convert column names to snake_case and remove leading/trailing whitespaces
+    new_columns = [snake_small_case(column.strip()) for column in read_file.columns]
+    read_file.columns = new_columns
+
     for column in new_columns:
-        read_file[column] = [read_file[column][i].strip() \
-                             if isinstance(read_file[column][i], str) \
-                             else read_file[column][i] \
-                             for i in range(0, len(read_file))]
+        # Remove leading/trailing whitespaces from each cell value
+        read_file[column] = [value.strip() if isinstance(value, str) else value for value in read_file[column]]
 
-    read_file.to_csv (f'dataset/data/{csv_name}.csv', \
-                    index = None, \
-                    header=True, \
-                    sep = ',', \
-                    decimal = ',', \
-                    encoding = 'utf-8-sig', \
-                    na_rep = "",
-                    mode='a') # Append mode
+    # Save to CSV with append mode (but overwrite existing rows with duplicates)
+    read_file.to_csv(f'dataset/data/{file.split(".xls")[0]}.csv' if aba is None else f'dataset/data/{aba}.csv',
+                     index=False, header=True, sep=',', decimal=',', encoding='utf-8-sig', na_rep="", mode='w')  # Change mode to 'w' to overwrite duplicates
+
 
 def snake_small_case(column):
-  column_lower = column.lower()
-  column_unidecode = unidecode(column_lower)
-  column_alphanumeric = re.sub('[^A-Za-z0-9]+', ' ', column_unidecode)
-  column_split = column_alphanumeric.split(' ')
-  column_empty = [x for x in column_split if x != '']
-  column = '_'.join(column_empty)
-  return column
+    column_lower = column.lower()
+    column_unidecode = unidecode(column_lower)
+    column_alphanumeric = re.sub('[^A-Za-z0-9]+', ' ', column_unidecode)
+    column_split = column_alphanumeric.split(' ')
+    column_empty = [x for x in column_split if x != '']
+    column = '_'.join(column_empty)
+    return column
+
 
 if __name__ == '__main__':
     find_all_files()
+
